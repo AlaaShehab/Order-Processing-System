6540f1c3c93c130bcda19e438e5697b98add8aa4
Alaa Shehab <alaa.shehab.95@gmail.com>
2019-05-12 00:08:03 +0200

    Update HomePageController.java
    Add image background

--- src/BookStore/HomePageController.java
+++ src/BookStore/HomePageController.java
@@ -8,16 +8,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomePageController implements Initializable {
+    @FXML private GridPane grid;
     private DBManager db;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         db = DBManager.getInstance();
+        FileInputStream inputstream = null;
+        try {
+            inputstream = new FileInputStream("D:\\Java Workspace\\BookStoreGUI\\Bookstore.jpg");
+        } catch (Exception e) {
+
+        }
+
+        Image image = new Image (inputstream);
+        grid.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.REPEAT,
+                BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT)));
     }
 
     @FXML
